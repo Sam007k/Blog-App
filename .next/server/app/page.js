0d0508659e643414389f2a4fb5b419459cbab544
Sameer <58684781+Sam007k@users.noreply.github.com>
2024-01-03 12:10:02 +0000

    build-changes

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -345,7 +345,7 @@ Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_
 
 /***/ }),
 
-/***/ 8322:
+/***/ 2096:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3380, 23));
